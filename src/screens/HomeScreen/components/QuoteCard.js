43951a18ec11b120ea4aa70161205ad74bc35a28
DharmikSonanai <dharmiksonani3001@gmail.com>
2025-05-30 22:18:18 +0530

    30/05/2025

--- src/screens/HomeScreen/components/QuoteCard.js
+++ src/screens/HomeScreen/components/QuoteCard.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { memo } from 'react'
+import { Animated, StyleSheet, Text } from 'react-native'
+import React, { memo, useEffect, useRef } from 'react'
 import { ResponsiveSizeWp } from '../../../helpers/responsive'
 import { COLOR } from '../../../helpers/colors'
 import { elevation_2 } from '../../../helpers/styles'
@@ -10,14 +10,28 @@ const QuoteCard = ({
     progress = 0,
 }) => {
 
+    const animation = useRef(new Animated.Value(0)).current;
     const quote = `"Do not pray for an easy life, pray for strength to endure a different one"`;
 
+    useEffect(() => {
+        Animated.timing(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+            duration: 500,
+        }).start();
+    }, [])
+
+    const scale = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+    })
+
     return (
-        <View style={[styles.Container, elevation_2, { shadowColor: COLOR.BLACK }]}>
+        <Animated.View style={[styles.Container, elevation_2, { shadowColor: COLOR.BLACK }, { transform: [{ scale: scale }] }]}>
             <Text style={styles.TitleText} numberOfLines={1}>Today's Quote</Text>
             <Text style={styles.QuoteText}>{quote}</Text>
             <ProgressLine data={progress} />
-        </View>
+        </Animated.View>
     )
 }
 
