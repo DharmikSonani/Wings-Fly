416799f4851d7ebb550eb98637f7b07dcf61f869
DharmikSonanai <dharmiksonani3001@gmail.com>
2025-05-30 15:28:44 +0530

    Setup

---
+++ src/helpers/assets.js
@@ -0,0 +1,16 @@
+export const AppIcon = require('../assets/icons/app-icon.png');
+
+export const WingsFlyLogo = require('../assets/images/wings-fly.png');
+
+export const Task1Icon = require('../assets/icons/task/task-icon-1.png');
+export const Task2Icon = require('../assets/icons/task/task-icon-2.png');
+export const Task3Icon = require('../assets/icons/task/task-icon-3.png');
+export const Task4Icon = require('../assets/icons/task/task-icon-4.png');
+export const Task5Icon = require('../assets/icons/task/task-icon-5.png');
+export const Task6Icon = require('../assets/icons/task/task-icon-6.png');
+
+export const BrainIcon = require('../assets/icons/options/brain-icon.png');
+export const GoalIcon = require('../assets/icons/options/goal-icon.png');
+export const TaskIcon = require('../assets/icons/options/task-icon.png');
+export const TickIcon = require('../assets/icons/options/tick-icon.png');
+
